670eb6a4e361fcec29548c31ab6feb69ba51e80c
Abhishek Yonghang <ayongh1@gmail.com>
2020-06-11 17:29:25 -0400

    fixing the 404 route and allcontent route

--- server.js
+++ server.js
@@ -43,9 +43,17 @@ app.use('/render/class',render_Class);
 app.use('/course', Course)
 app.use('/user/info', user_info)
 
+
+
+
 if(process.env.NODE_ENV === 'production')
 {
     app.use(express.static('client/build'))
+
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
 }
+
 
 app.listen(port, console.log(`server is starting at ${port}`))
