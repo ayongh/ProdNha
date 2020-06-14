dce8591be2b6ff693b95280b7bc3cec430e5d1c7
Abhishek Yonghang <ayongh1@gmail.com>
2020-06-14 00:30:12 -0400

    landing page with login changes

---
+++ client/src/component/public/component/loginModel.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import {Link,Redirect} from 'react-router-dom'
+
+import axios from 'axios'
+import Recaptcha from 'react-google-invisible-recaptcha';
+
+import { Icon } from 'react-icons-kit'
+import {close} from 'react-icons-kit/fa/close'
+import {spinner2} from 'react-icons-kit/icomoon/spinner2'
+
+
+import {connect} from 'react-redux'
+import {Actionlogin, ActionLoading, ActionError} from '../../../redux/Action/loginAction' //'../../redux/Action/loginAction'
+import {ActionUserIntialize} from '../../../redux/Action/userinfoAction'
+
+class loginModel extends Component
+{
+    
+   
+
+    render()
+    {
+        
+
+        return (
+            <div className="login_model_container">
+                <div className="login_model_left_container">
+                    <h1>login</h1>
+                </div>
+                <div className="login_model_right_container">
+
+                </div>
+            </div>
+        )//return
+    }  
+}
+
+//access all the state
+const mapToState = (state) =>{
+    return {
+        state:state
+    }
+}
+
+export default connect(mapToState,{Actionlogin,ActionLoading,ActionError,ActionUserIntialize}) (loginModel);
--- client/src/component/public/login.js
+++ client/src/component/public/login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Recaptcha from 'react-google-invisible-recaptcha';
+import axios from 'axios'
 
 import { Icon } from 'react-icons-kit'
 import {close} from 'react-icons-kit/fa/close'
@@ -11,7 +12,8 @@ import {connect} from 'react-redux'
 import {Actionlogin, ActionLoading, ActionError} from '../../redux/Action/loginAction'
 import {ActionUserIntialize} from '../../redux/Action/userinfoAction'
 
-import axios from 'axios'
+import LoginModel from '../../component/public/component/loginModel'
+
 
 class login extends Component
 {
@@ -63,6 +65,8 @@ class login extends Component
 
     }
 
+
+
     closealert(){
         document.getElementById("alert").style.display="none"
     }
@@ -113,6 +117,17 @@ class login extends Component
         })
     }
 
+    //Model Close
+    modelClose()
+    {
+        document.getElementById('myModal').style.display="none"
+    }
+
+    loginModelOpen()
+    {
+        document.getElementById('myModal').style.display="block"
+    }
+
     render()
     {
         //Checks error and display it errorElement
@@ -150,54 +165,84 @@ class login extends Component
         }
 
         return (
-            <div className="loginEntire">
-                <div className="login_body">
-                <div className="login_left">
-                    <div className="login_left_outer_container">
-                        <h3>Create New User</h3>
-                        <label>
-                            If you dont have a user name and password created please 
-                            do so now by clicking the button below. Thank you!
-                        </label>
-                        <Link to={"/signup"}>
-                            <button type="button" className="signup btn" href="/signup">Sign up</button>
-                        </Link>
+            <div>
+                <div className="login_nav">
+                    <div className="login_nav_wrapper">
+                        <a className="login_option" onClick={this.loginModelOpen}>Login</a>
+                        <a className="login_option" href="/signup">Signup</a>
                     </div>
                 </div>
-                <div className="login_right">
-                    <div className="login_right_outer_container">
-                        <div className="login_right_inner_container">
-                            <h2>Login</h2>
-    
-                            <form onSubmit={this.handleSubmit}>
-                                <div className="login_login_right_inner_container_elem">
-                                    <label className="login_label">User Name</label>
-                                    <input id="username" onChange={this.handleChange}  className="txt" type="text" placeholder="User Name"></input>
-                                </div>
-                                
-                                <div className="login_login_right_inner_container_elem">
-                                    <label className="login_label">password</label>
-                                    <input id="password" onChange={this.handleChange}  className="txt" type="password" placeholder="password"></input>
-                                </div>
-                                {errorElement}
-                                <button id="submit_btn" className="btn" type="submit">
-                                    <span id="loginLoad"><Icon className="loginIcon" id="loginicon" icon={spinner2}></Icon></span>
-                                    <span id="loginText">Login</span>
 
-                                </button>
-                            </form>
-                            
-                            <p> I forgot my <Link to={"/pswdreset"}>password</Link></p>
-                        </div>
+                <header className="v-header container">
+                    <div className="fullscreen-video-wrap">
+                        <video src="https://firebasestorage.googleapis.com/v0/b/nhadb-c07ce.appspot.com/o/video%2Fvideoplayback%20(1).mp4?alt=media&token=012d7e51-680a-4773-b9ae-1b483e4966ae" autoPlay={true} loop={true}></video>
                     </div>
-                    
+                    <div className="header-overlay"></div>
+                    <div className="header-content text-md-center">
+                        <h1>Welcome <span className="welcome_small_content">Everyone <span style={{color:'red'}}>!</span></span></h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id temporibus perferendis necessitatibus numquam amet impedit explicabo? Debitis quasi ullam aperiam!</p>
+                        <a className="btn" href="#about">Find Out More</a>
+                    </div>
+                </header>
+        
+                <section className="section section-a" id="about">
+                    <div className="container">
+                    <h2>About</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, impedit amet minima iste autem cumque et maiores blanditiis doloribus aut dolorum quaerat non est voluptatum, tempore ut dolorem voluptas quod quae accusantium, ex inventore ducimus. Beatae mollitia exercitationem, quam similique, consectetur ratione reprehenderit delectus neque eligendi facere soluta dolor ducimus!</p>
+                    </div>
+                </section>
+        
+                <section className="section section-b">
+                    <div className="container">
+                    <h2>Section B</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, impedit amet minima iste autem cumque et maiores blanditiis doloribus aut dolorum quaerat non est voluptatum, tempore ut dolorem voluptas quod quae accusantium, ex inventore ducimus. Beatae mollitia exercitationem, quam similique, consectetur ratione reprehenderit delectus neque eligendi facere soluta dolor ducimus!</p>
+                    </div>
+                </section>
+
+                <div id="myModal" class="modal">
+                    <div class="modal-content">
+                        <div className="login_model_wrapper">
+                            <div className="model_container left">
+                                <h2>Notification</h2>
+                                <p>welcome to the page new notification and other things will be here</p>
+                            </div>
+
+                            <div className="model_container right">
+                                <span class="close" onClick={this.modelClose}>&times;</span>
+                                <div className="login_right_inner_container">
+                                    <h2>Login</h2>
+            
+                                    <form onSubmit={this.handleSubmit}>
+                                        <div className="login_login_right_inner_container_elem">
+                                            <label className="login_label">User Name</label>
+                                            <input id="username" onChange={this.handleChange}  className="txt" type="text" placeholder="User Name"></input>
+                                        </div>
+                                        
+                                        <div className="login_login_right_inner_container_elem">
+                                            <label className="login_label">password</label>
+                                            <input id="password" onChange={this.handleChange}  className="txt" type="password" placeholder="password"></input>
+                                        </div>
+                                        {errorElement}
+                                        <button id="submit_btn" className="btn" type="submit">
+                                            <span id="loginLoad"><Icon className="loginIcon" id="loginicon" icon={spinner2}></Icon></span>
+                                            <span id="loginText">Login</span>
+
+                                        </button>
+                                    </form>
+                                    <p> I forgot my <Link to={"/pswdreset"}>password</Link></p>
+                                </div>
+                            </div>
+                        </div>
+                       
+
+                    </div>
+
                 </div>
                 <Recaptcha
                 ref={ ref => this.recaptcha = ref }
                 //**************************************************DANGER remove site key to saftey *********************************************************************
                 sitekey='6LdhWNsUAAAAAKIeVaOGdY3HCKy5Siva9emmZDl6'
                 onResolved={ this.onResolved } />
-                </div>
             </div>
             
         )//return
